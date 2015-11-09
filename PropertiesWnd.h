be69ab9e74536873565e3a1d1db948884bc06e17
PatriciaMalone <Patricia.Malone@advancedcomputersoftware.com>
2015-11-09 14:16:41 +0000

    dev/test3 [TEST-7]:   An issue test

--- PropertiesWnd.h
+++ PropertiesWnd.h
@@ -53,7 +53,7 @@ protected:
 	afx_msg void OnSettingChange(UINT uFlags, LPCTSTR lpszSection);
 
 	DECLARE_MESSAGE_MAP()
-	//fff llll hh  ff fff
+	//fff llll hh  ff fff ddd  dd
 
 	void InitPropList();
 	void SetPropListFont();
