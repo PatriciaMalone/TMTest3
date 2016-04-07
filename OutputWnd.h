5d847d05c9c8cdd068d0b1ea85fa37bfa8341968
PatriciaMalone <PatriciaMalone@users.noreply.github.com>
2016-04-07 12:45:50 +0100

    Update OutputWnd.h

--- OutputWnd.h
+++ OutputWnd.h
@@ -1,5 +1,6 @@
 //
 #pragma once
+//Introduced clash
 //hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
 /////////////////////////////////////////////////////////////////////////////
 // COutputList window
